33cf60a1cbbac9579211a087ebd7c47146fa7ad9
Collins <mussocollins@gmail.com>
2022-10-28 11:51:00 +0300

    Create rail project

---
+++ app/models/coment.rb
@@ -0,0 +1,9 @@
+class Coment < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
+  after_save :update_coments_counter
+
+  def update_coments_counter
+    post.increment!(:coments_counter)
+  end
+end
---
+++ db/migrate/20221028081629_create_coments.rb
@@ -0,0 +1,10 @@
+class CreateComents < ActiveRecord::Migration[7.0]
+  def change
+    create_table :coments do |t|
+      t.references :author, index: true, foreign_key: {to_table: :users}
+      t.references :post, index: true, foreign_key: {to_table: :posts}
+      t.text :text
+      t.timestamps
+    end
+  end
+end
