7189bd84ea508f71dfe578a909eba7c83e8cc026
Collins <mussocollins@gmail.com>
2022-10-28 20:35:52 +0300

    Add changes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
-  after_save :update_coments_counter
+  after_save :update_comments_counter
 
   def update_coments_counter
-    post.increment!(:coments_counter)
+    post.increment!(:comments_counter)
   end
 end
