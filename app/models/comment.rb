daebbe551590758af3adc935b7a9d81b799171b1
Collins <mussocollins@gmail.com>
2022-10-28 13:52:54 +0300

    Change comment model as requested by the reviewer

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,8 @@
-class Coment < ApplicationRecord
+class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
   after_save :update_coments_counter
-
+  
   def update_coments_counter
     post.increment!(:coments_counter)
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,24 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_28_081629) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_28_104505) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "coments", force: :cascade do |t|
+  create_table "comments", force: :cascade do |t|
     t.bigint "author_id"
     t.bigint "post_id"
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["author_id"], name: "index_coments_on_author_id"
-    t.index ["post_id"], name: "index_coments_on_post_id"
-  end
-
-  create_table "comments", force: :cascade do |t|
-    t.text "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_comments_on_author_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
   create_table "likes", force: :cascade do |t|
@@ -58,8 +52,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_28_081629) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "coments", "posts"
-  add_foreign_key "coments", "users", column: "author_id"
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users", column: "author_id"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users", column: "author_id"
   add_foreign_key "posts", "users", column: "author_id"
