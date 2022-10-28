d850c89a4f492b3c2d6820ad2101b2c4936e0c76
Collins <mussocollins@gmail.com>
2022-10-28 13:56:17 +0300

    Correct linters

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
   after_save :update_coments_counter
-  
+
   def update_coments_counter
     post.increment!(:coments_counter)
   end
