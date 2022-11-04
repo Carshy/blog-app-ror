95cdfefbd730682232d0633d90d161a4666f974c
Collins <mussocollins@gmail.com>
2022-11-04 14:42:24 +0300

    Add view actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,9 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @user = User.find(params[:user_id])
   end
 
   def show
-    @post = Post.first
+    @post = Post.find(params[:id])
   end
 end
