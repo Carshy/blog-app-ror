dda76c24b63aad2b03b2ee10922aa09419609f57
Collins <mussocollins@gmail.com>
2022-11-11 20:26:03 +0300

    Add authorization as suggested by the reviewer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
   before_action :update_allowed_parameters, if: :devise_controller?
 
   protected
@@ -11,9 +11,4 @@ class ApplicationController < ActionController::Base
       u.permit(:name, :surname, :bio, :photo, :email, :password, :current_password)
     end
   end
-
-  # Catch all CanCan errors and alert the user of the exception
-  # rescue_from CanCan::AccessDenied do |exception|
-  #   redirect_to root_url, alert: exception.message
-  # end
 end
